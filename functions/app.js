d602eda0d9a3d44e3a1cd559b16183206fb0c2d8
shaurya <sgarg7@ncsu.edu>
2018-02-26 23:26:57 -0500

    functions

---
+++ functions/app.js
@@ -0,0 +1,11 @@
+function greet(){
+    console.log('hi');
+}
+
+greet.language = 'english';
+//will give the function text
+console.log(greet);
+
+console.log(greet.language);
+
+console.log(greet.name);
