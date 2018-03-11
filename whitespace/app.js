5dcf35c0b78ef1adedd80186f3d3fdbc4d5866b4
shaurya <sgarg7@ncsu.edu>
2018-03-11 01:03:19 -0500

    white space

---
+++ whitespace/app.js
@@ -0,0 +1,33 @@
+//Whitespace - carriage returns , tabs or spaces
+//Js is very liberal in terms of allowing white spaces
+
+var firstname, lastname, language;
+
+var person = {
+    firstname: 'Shaurya',
+    lastname: 'Garg'
+}
+
+console.log(person);
+
+
+
+var //We can even put comments here
+    firstname, 
+    
+    //Also here
+    lastname, 
+    
+    //Also here
+    language;
+
+var person = {
+    //Oops
+    firstname: 'Shaurya',
+
+    // the last name
+    //(always required)
+    lastname: 'Garg'
+}
+
+console.log(person);
